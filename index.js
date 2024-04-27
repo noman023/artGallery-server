2235bbba1bdf3ae87fb2527257ac19173cd56fab
Mojammel Noman <molla.noman23@gmail.com>
2024-04-28 03:38:53 +0600

    dotenv file added

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middlewares
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hello");
+// mongodb atlas connection uri
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASS}@cluster0.faeme9d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("assignment10");
+    git;
+    const items = database.collection("items");
+
+    app.get("/items", async (req, res) => {
+      const itemsCollection = items.find();
+      const result = await itemsCollection.toArray();
+
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
-  console.log(" server running");
+  console.log(`Server Running on port ${port}`);
 });
