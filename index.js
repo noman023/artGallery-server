378066d44a9972c1c662f1f4c7c2b27e4c91c79a
Mojammel Noman <molla.noman23@gmail.com>
2024-04-30 19:56:24 +0600

    added api for finding single data

--- index.js
+++ index.js
@@ -38,6 +38,14 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/items/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await items.findOne(query);
+
+      res.send(result);
+    });
+
     app.post("/items", async (req, res) => {
       const item = req.body;
       item.createdAt = new Date();
